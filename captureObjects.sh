c48c9585b501dc3b02d6932c5c4caaa9df4577fb
Audris Mockus <audris@utk.edu>
2018-11-15 18:16:58 +0000

    add targets

--- captureObjects.sh
+++ captureObjects.sh
@@ -4,9 +4,9 @@ repo=$1
 rp0=$(echo $repo|sed 's|.*//||')
 rpn=$(echo $rp0|sed 's|[^/]*/||')
 rpb=$(echo $rp0|sed 's|/[^/]*/[^/]*$||')
-rpp=$(echo $rp1|sed 's|/|_|')
+rpp=$(echo $rpn|sed 's|/|_|')
 git clone $repo $rpp
-/usr/bin/gitListSimp.sh $rpp | /usr/bin/classify $rp 2>> $rpp.olist.err | gzip > $rpp.olist.gz
+/usr/bin/gitListSimp.sh $rpp | /usr/bin/classify $rpp 2>> $rpp.olist.err | gzip > $rpp.olist.gz
 #now check
 zcat $rpp.olist.gz | grep ';commit;' | cut -d\; -f3 | gzip > $rpp.cs
 zcat $rpp.olist.gz | /usr/bin/cleanBlb.perl | /usr/bin/hasObj.perl | gzip > $rpp.todo
